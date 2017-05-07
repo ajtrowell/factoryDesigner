390ce0b3881f0fcb6f205bc1310902f79338ab41
AJTrowell <ajtrowell@gmail.com>
2017-05-07 02:32:47 -0500

    Cleaned up main.js, ran a few visible functions.

--- main.js
+++ main.js
@@ -1,36 +1,12 @@
-// Some objects are uncreated "prime" objects, such as ironField or waterPool.
-// Temporarily, I will treate ironPlate and steelPlate as "prime," to 
-// help create a useful structure more quickly.  This is a 
-// temporary hack to help with the proof of concept
 
 
-// Store factory info as objects
-var gameItems =[]; // Object containing game items as properties
 
-function GameItem(name, craftTime, recipeArray, outputQty) {
-    // name is a string
-    // craftTime is a number
-    // recipeArray contains n sub arrays, each with a quantity (number) and a 
-    // name (string)
-    // outputQty (number) is the yield of the recipe.
-    this.name = name;
-    this.craftTime = craftTime;
-    this.recipeArray = recipeArray; // (should do error checking to verify format)
-        if (this.recipeArray.length == 0) { 
-            showError("Error, recipeArray cannot be empty.");
-        }
-        for(let i = 0; i<this.recipeArray.length; ++i) {
-            if (this.recipeArray[i].length != 2) {
-               showError("Error, recipeArray not formed correctly.");
-            }
-        }
+items.list();
+items.showRecipe("pumpJack");
+var reqStruct = items.getRecipeRequirements("purpleScience",1);
+
+
     
-    if (typeof outputQty === "undefined") // handle optional argument outputQty
-        { this.outputQty = 1;} // default value
-    else 
-        {this.outputQty = outputQty;}
-    
-}
 
 function show(arg) {
     console.log(arg);
